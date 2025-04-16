2b318b6a5d03ae08777d7d781e8ab5d0938cbb79
kmart138 <kmart138@ucsc.edu>
2025-04-16 15:58:54 -0700

    lkjhg

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,17 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Flappy Bird Clone
+// Kalani Martin
+// Date: April 16, 2025
+
+// Create a starting screen
+   // Add a Start button, waiting for the player to press it
+// After player presses start, bird begins to fall at a constant speed
+   // If the player presses space, make the bird move up a little
+   // Keep track of the score. Each second on screen equals a point. 
+// Every 2 seconds, create two pipes, one extending from the bottom of the screen and the other from the top of the screen, with a gap the size of two birds length-wise in between them
+// When the bird sprite hits a pipe, the game ends.
+// Show the score on screen, with a Restart button below it 
+// Return back to the starting screen after the player presses Restart
+
 
 // Constants
 
