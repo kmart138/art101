a174aec3148f4ebfe18f399f6332a8c6deeba052
kmart138 <kmart138@ucsc.edu>
2025-04-24 18:58:29 -0700

    ert

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,33 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - making a array and using it with objects
+// Author: Kalani Martin
+// Date: 4/24/2025
 
 // Constants
 
-// Functions
+// Functions MT
+myMainRide = {
+  make: "Toyota",
+  model: "Sienna",
+  color: "Grey",
+  year: 2006,
+  age: function() {
+    return 2025 - this.year;
+  }
+};
+
+
+myTransport = ["Toyota Sienna", "Bus", "Walking", "Bus"];
+
+// outputs
+
+document.writeln("Kalani's Ways of Getting Around: " + myTransport + "<br>");
+
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
