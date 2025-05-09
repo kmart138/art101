15ee6fd6a9a5258b61717728e36cf62fbf3e677b
kmart138 <kmart138@ucsc.edu>
2025-05-08 18:50:18 -0700

    cv
    g

---
+++ lab9/js/lab.js
@@ -0,0 +1,66 @@
+// index.js - Lab 9: Libraries & jQuery
+// Author: Kalani Martin
+// Date: 5/8/25
+
+// Constants
+
+// Functions
+
+// //button to refer to challange
+
+// $("#challenge").append("<button id='button-challenge'>Problem Button</button>");
+
+// //A click is used to listen in this case for a event to happen upon press which is a listener
+
+// $("#button-challenge").click(function(){
+//   $("#challenge").toggleClass("special");
+// });
+
+// // CYCLE THIS FOR EVERY SECTION
+
+// // BUTTON FOR REFLECTION
+// $("#reflection").append("<button id='button-reflection'>Problem Button</button>");
+
+// $("#button-reflection").click(function(){
+//   $("#reflection").toggleClass("special");
+// });
+// // BUTTON FOR PROBLEMS
+
+// $("#probs").append("<button id='button-probs'>Problem Button</button>");
+
+// $("#button-probs").click(function(){
+//   $("#probs").toggleClass("special");
+// });
+// // BUTTON FOR PROBLEMS
+
+// $("#res").append("<button id='button-'>Problem Button</button>");
+
+// $("#button-res").click(function(){
+//   $("#res").toggleClass("special");
+// });
+
+// BUTTON FOR RESULTS
+
+
+//TASK X
+
+$(".minor-section").append("<button class='button-X'>Problem X Button</button>");
+
+$(".button-X").click(function(){
+  $(this).parent().toggleClass("special");
+})
+
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
